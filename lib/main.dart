e7f3994cd50ad243ae438fe546a7e699e875708e
nvrr <sureshvarma809@gmail.com>
2020-09-16 12:59:26 +0530

    Flutter Layout-4 GridView

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
-
-
 import 'package:flutter/material.dart';
-// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
- // debugPaintSizeEnabled = true; // Set to true for visual layout
+  debugPaintSizeEnabled = false; // Set to true for visual layout
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
+  static final showGrid = true; // Set to false to show ListView
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,42 +17,53 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Flutter layout demo'),
         ),
-        body: Center(child: _buildImageColumn())
+        body: Center(child: showGrid ? _buildGrid() : _buildList()),
       ),
     );
+
   }
 
-  // #docregion column
-  Widget _buildImageColumn() => Container(
-        decoration: BoxDecoration(
-          color: Colors.black26,
-        ),
-        child: Column(
-          children: [
-            _buildImageRow(1),
-            _buildImageRow(3),
-          ],
-        ),
-      );
-  // #enddocregion column
+  Widget _buildGrid() => GridView.extent(
+    maxCrossAxisExtent: 150,
+    padding: const EdgeInsets.all(4),
+    mainAxisSpacing: 4,
+    crossAxisSpacing: 4,
+    children: _buildGridTileList(6),
+  );
+  
 
-  // #docregion row
-  Widget _buildDecoratedImage(int imageIndex) => Expanded(
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(width: 10, color: Colors.black38),
-            borderRadius: const BorderRadius.all(const Radius.circular(8)),
-          ),
-          margin: const EdgeInsets.all(4),
-          child: Image.asset('images/pic$imageIndex.jpg'),
-        ),
-      );
+  List<Container> _buildGridTileList(int count) => List.generate(
+    count, (i) => Container(child: Image.asset('images/pic$i.jpg'))
+  );
 
-  Widget _buildImageRow(int imageIndex) => Row(
+   Widget _buildList() => ListView(
         children: [
-          _buildDecoratedImage(imageIndex),
-          _buildDecoratedImage(imageIndex + 1),
+          _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
+          _tile('The Castro Theater', '429 Castro St', Icons.theaters),
+          _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
+          _tile('Roxie Theater', '3117 16th St', Icons.theaters),
+          _tile('United Artists Stonestown Twin', '501 Buckingham Way',
+              Icons.theaters),
+          _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
+          Divider(),
+          _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
+          _tile('Emmy\'s Restaurant', '1923 Ocean Ave', Icons.restaurant),
+          _tile(
+              'Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant),
+          _tile('La Ciccia', '291 30th St', Icons.restaurant),
         ],
       );
-  // #enddocregion row
+
+  ListTile _tile(String title, String subtitle, IconData icon) => ListTile(
+        title: Text(title,
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 20,
+            )),
+        subtitle: Text(subtitle),
+        leading: Icon(
+          icon,
+          color: Colors.blue[500],
+        ),
+      );
 }
