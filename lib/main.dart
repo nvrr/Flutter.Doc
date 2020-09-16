8853a475292e7225fd91e909995b3c50b5017563
nvrr <sureshvarma809@gmail.com>
2020-09-16 12:02:50 +0530

    Flutter Layout-3

--- lib/main.dart
+++ lib/main.dart
@@ -9,125 +9,51 @@ void main() {
 }
 
 
-
-// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-     Color color = Theme.of(context).primaryColor;
-
-    Widget buttonSection = Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          _buildButtonColumn(color, Icons.call, 'CALL'),
-          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-          _buildButtonColumn(color, Icons.share, 'SHARE'),
-        ],
-      ),
-    );
-
     return MaterialApp(
-      title: 'Flutter layout Demo',
+      title: 'Flutter layout demo',
       home: Scaffold(
         appBar: AppBar(
-          leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
+          title: Text('Flutter layout demo'),
         ),
-          title: Text('Flutter Layout'), centerTitle: true,
-          actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
+        body: Center(child: _buildImageColumn())
+      ),
+    );
+  }
+
+  // #docregion column
+  Widget _buildImageColumn() => Container(
+        decoration: BoxDecoration(
+          color: Colors.black26,
+        ),
+        child: Column(
+          children: [
+            _buildImageRow(1),
+            _buildImageRow(3),
+          ],
+        ),
+      );
+  // #enddocregion column
+
+  // #docregion row
+  Widget _buildDecoratedImage(int imageIndex) => Expanded(
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(width: 10, color: Colors.black38),
+            borderRadius: const BorderRadius.all(const Radius.circular(8)),
           ),
+          margin: const EdgeInsets.all(4),
+          child: Image.asset('images/pic$imageIndex.jpg'),
+        ),
+      );
+
+  Widget _buildImageRow(int imageIndex) => Row(
+        children: [
+          _buildDecoratedImage(imageIndex),
+          _buildDecoratedImage(imageIndex + 1),
         ],
-        ),
-        body: ListView(
-          children: <Widget>[
-            Image.asset(
-              'images/lake.jpg',
-              width: 600,
-              height: 240,
-              fit: BoxFit.cover
-            ),
-            titleSection,
-            buttonSection,
-            textSection
-          ],)
-      )
-    );
-  }
-
-  Widget titleSection = Container(
-
-    padding: const EdgeInsets.all(32),
-    child:Row(
-      children: [
-        Expanded(
-           child: Column(
-             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding:  const EdgeInsets.only(bottom: 8),
-                child: Text(
-                  'Oeschinen Lake Campground',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold
-                  )
-                  )
-              ),
-              Text(
-                'Kandersteg, Switzerland',
-                style: TextStyle(
-                  color: Colors.grey[500]
-                )
-                )
-            ],),
-        ),
-          Icon(Icons.star,
-          color: Colors.red[500]
-          ),
-          Text('41')
-      ],)
-  );
-
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-         margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color
-              )
-          ),)
-      ],
-    );
-  }
-
- 
-Widget textSection = Container(
-  padding: const EdgeInsets.all(32),
-  child: Text(
-    'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-        'Alps. Situated 1,578 meters above sea level, it is one of the '
-        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-        'half-hour walk through pastures and pine forest, leads you to the '
-        'lake, which warms to 20 degrees Celsius in the summer. Activities '
-        'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true
-        ),
-
-   
-  );
+      );
+  // #enddocregion row
 }
