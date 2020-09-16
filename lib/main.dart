d98061b42da39e27873ec7402d168d812dd305ea
nvrr <sureshvarma809@gmail.com>
2020-09-16 16:44:18 +0530

    Flutter Layout-Stack and Card

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  static final showGrid = true; // Set to false to show ListView
+  static final showCard = false; // Set to false to show Stack
 
   @override
   Widget build(BuildContext context) {
@@ -17,53 +17,70 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Flutter layout demo'),
         ),
-        body: Center(child: showGrid ? _buildGrid() : _buildList()),
+        body: Center(child: showCard ? _buildCard() : _buildStack()),
       ),
     );
-
   }
 
-  Widget _buildGrid() => GridView.extent(
-    maxCrossAxisExtent: 150,
-    padding: const EdgeInsets.all(4),
-    mainAxisSpacing: 4,
-    crossAxisSpacing: 4,
-    children: _buildGridTileList(6),
-  );
-  
-
-  List<Container> _buildGridTileList(int count) => List.generate(
-    count, (i) => Container(child: Image.asset('images/pic$i.jpg'))
-  );
-
-   Widget _buildList() => ListView(
-        children: [
-          _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
-          _tile('The Castro Theater', '429 Castro St', Icons.theaters),
-          _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
-          _tile('Roxie Theater', '3117 16th St', Icons.theaters),
-          _tile('United Artists Stonestown Twin', '501 Buckingham Way',
-              Icons.theaters),
-          _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
-          Divider(),
-          _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
-          _tile('Emmy\'s Restaurant', '1923 Ocean Ave', Icons.restaurant),
-          _tile(
-              'Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant),
-          _tile('La Ciccia', '291 30th St', Icons.restaurant),
-        ],
-      );
-
-  ListTile _tile(String title, String subtitle, IconData icon) => ListTile(
-        title: Text(title,
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 20,
-            )),
-        subtitle: Text(subtitle),
-        leading: Icon(
-          icon,
-          color: Colors.blue[500],
+  // #docregion Card
+  Widget _buildCard() => SizedBox(
+      height: 210,
+      child: Card(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('1625 Main Street',
+                  style: TextStyle(fontWeight: FontWeight.w500)),
+              subtitle: Text('My City, CA 99984'),
+              leading: Icon(
+                Icons.restaurant_menu,
+                color: Colors.blue[500],
+              ),
+            ),
+            Divider(),
+            ListTile(
+              title: Text('(408) 555-1212',
+                  style: TextStyle(fontWeight: FontWeight.w500)),
+              leading: Icon(
+                Icons.contact_phone,
+                color: Colors.blue[500],
+              ),
+            ),
+            ListTile(
+              title: Text('costa@example.com'),
+              leading: Icon(
+                Icons.contact_mail,
+                color: Colors.blue[500],
+              ),
+            ),
+          ],
         ),
-      );
+      ),
+    );
+  // #enddocregion Card
+
+  // #docregion Stack
+  Widget _buildStack() => Stack(
+      alignment: const Alignment(0.6, 0.6),
+      children: [
+        CircleAvatar(
+          backgroundImage: AssetImage('images/pic.jpg'),
+          radius: 100,
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.black45,
+          ),
+          child: Text(
+            'Mia B',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+  // #enddocregion Stack
 }
