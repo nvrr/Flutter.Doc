7a1536f70e08d992250e3364f10a0beed5119f67
nvrr <sureshvarma809@gmail.com>
2020-09-22 14:20:54 +0530

    The ListTile layout isn't exactly what I want:::    If the way ListTile pads and positions its elements isn't quite what you're looking for, it's easy to create custom list items with a combination of other widgets, such as Rows and Columns.

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,142 @@
+// Flutter code sample for ListTile
+
+// Here is an example of a custom list item that resembles a Youtube related
+// video list item created with [Expanded] and [Container] widgets.
+//
+// ![Custom list item a](https://flutter.github.io/assets-for-api-docs/assets/widgets/custom_list_item_a.png)
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+/// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
-
-  final appTitle = 'ListView Demo';
+  static const String _title = 'Flutter Code Sample';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: appTitle,
-      home: MyHomePage(title: appTitle)
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: MyStatelessWidget(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class CustomListItem extends StatelessWidget {
 
+  const CustomListItem({
+    this.thumbnail,
+    this.title,
+    this.user,
+    this.viewCount
+  });
+
+  final Widget thumbnail;
   final String title;
-  MyHomePage({Key key, this.title}) : super(key : key);
-
-  final List<String> entries = <String>['A', 'B', 'C'];
-  final List<int> colorCodes = <int>[600, 400, 200];
+  final String user;
+  final int viewCount;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: ListView.separated(
-        padding: const EdgeInsets.all(8),
-        itemCount: entries.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 50,
-            color:Colors.amber[colorCodes[index]],
-            
-          child: Center(child: Text('Entry ${entries[index]}'),),
-          );
-        },
-       separatorBuilder: (BuildContext context, int index) => const Divider(),
-      )
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex:2,
+            child: thumbnail
+          ),
+          Expanded(
+            flex:3,
+            child: _VideoDescription(
+              title: title,
+              user: user,
+              viewCount: viewCount
+            ),
+          ),
+          const Icon(
+            Icons.more_vert,
+            size: 16.0
+          )
+        ],),
     );
   }
 }
 
-/*
- ListView.builder(
-        padding: const EdgeInsets.all(8),
-        itemCount: entries.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 50,
-            color:Colors.amber[colorCodes[index]],
-            
-          child: Center(child: Text('Entry ${entries[index]}'),),
-          );
-        },
-      )
-*/
+class _VideoDescription extends StatelessWidget {
 
-/*
-ListView(
-        padding: const EdgeInsets.all(8),
-        children: [
-          Container(
-           child: const Center(child: Text('Entry A')),
-           height: 50,
-           color: Colors.amber[600]
+  const _VideoDescription({
+    Key key,
+    this.title, 
+    this.user,
+    this.viewCount
+  }) : super(key : key);
+
+ final String title;
+  final String user;
+  final int viewCount;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(5.0, 0, 0, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 14.0
+            )
           ),
-           Container(
-           child: const Center(child: Text('Entry B')),
-           height: 50,
-           color: Colors.amber[400]
+          const Padding(padding: EdgeInsets.symmetric(vertical: 2.0)),
+          Text(
+            user,
+            style: const TextStyle(fontSize: 10.0)
           ),
-           Container(
-           child: const Center(child: Text('Entry c')),
-           height: 50,
-           color: Colors.amber[200]
+          const Padding(padding: EdgeInsets.symmetric(vertical: 1.0)),
+          Text(
+            '$viewCount views',
+            style: const TextStyle(fontSize: 10.0)
           )
         ],
-      )
-      */
+      ),
+    );
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+
+  MyStatelessWidget({Key  key}):super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.all(8),
+      itemExtent: 106.0,
+      children: <CustomListItem>[
+        CustomListItem(
+          user: "NVRR",
+          viewCount: 9393,
+          thumbnail: Container(
+            decoration: const BoxDecoration(color: Colors.blue),
+          ),
+           title: 'The Flutter Tutorials'
+        ),
+         CustomListItem(
+          user: "NVR",
+          viewCount: 936,
+          thumbnail: Container(
+            decoration: const BoxDecoration(color: Colors.amber),
+          ),
+           title: 'The Flutter Begginer Tutorials'
+        )
+      ],
+    );
+  }
+}
