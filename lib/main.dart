1ee6f72e6c6df5334e75b2a0becfed4ff0a8f0c5
nvrr <sureshvarma809@gmail.com>
2020-09-23 12:17:43 +0530

    This example shows how to create the same grid as the previous example using a CustomScrollView and a SliverGrid.

--- lib/main.dart
+++ lib/main.dart
@@ -15,45 +15,66 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text(appTitle)
         ),
-        body:GridView.count(
-  primary: false,
-  padding: const EdgeInsets.all(20),
-  crossAxisSpacing: 10,
-  mainAxisSpacing: 10,
-  crossAxisCount: 2,
-  children: <Widget>[
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text("He'd have you all unravel at the"),
-      color: Colors.teal[100],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Heed not the rabble'),
-      color: Colors.teal[200],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Sound of screams but the'),
-      color: Colors.teal[300],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Who scream'),
-      color: Colors.teal[400],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Revolution is coming...'),
-      color: Colors.teal[500],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Revolution, they...'),
-      color: Colors.teal[600],
-    ),
-  ],
-)
+        body: CustomScrollView(
+          primary: false,
+          slivers: <Widget>[
+            SliverPadding(
+              padding: const EdgeInsets.all(20),
+              sliver: SliverGrid.count(
+                crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text("He'd have you all unravel at the"),
+            color: Colors.green[100],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('Heed not the rabble'),
+            color: Colors.green[200],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('Sound of screams but the'),
+            color: Colors.green[300],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('Who scream'),
+            color: Colors.green[400],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('Revolution is coming...'),
+            color: Colors.green[500],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('Revolution, they...'),
+            color: Colors.green[600],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('Revolution, they...'),
+            color: Colors.green[600],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('Revolution, they...'),
+            color: Colors.green[600],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('Revolution, they...'),
+            color: Colors.green[600],
+          ),
+        ],
+              )
+            )
+          ]
+        )
       )
     );
   }
