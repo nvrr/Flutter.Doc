1189d82c15bf74ffec8511340cf2a6f2e03f72a4
nvrr <sureshvarma809@gmail.com>
2020-09-23 16:12:27 +0530

    This example shows how to listen to page updates in TabBar and TabBarView when using DefaultTabController.

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,67 @@
+// Flutter code sample for TabController
+
+// This example shows how to listen to page updates in [TabBar] and [TabBarView]
+// when using [DefaultTabController].
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(TabBarDemo());
-}
+void main() => runApp(MyApp());
 
-class TabBarDemo extends StatelessWidget {
+/// This Widget is the main application widget.
+class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
+      title: _title,
+      home: MyStatelessWidget(),
+    );
+  }
+}
+
+final List<Tab> tabs = <Tab>[
+  Tab(text: 'Zeroth'),
+  Tab(text: 'First'),
+  Tab(text: 'Second'),
+];
+
+/// This is the stateless widget that the main application instantiates.
+class MyStatelessWidget extends StatelessWidget {
+  MyStatelessWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: tabs.length,
+      // The Builder widget is used to have a different BuildContext to access
+      // closest DefaultTabController.
+      child: Builder(builder: (BuildContext context) {
+        final TabController tabController = DefaultTabController.of(context);
+        tabController.addListener(() {
+          if (!tabController.indexIsChanging) {
+            // Your code goes here.
+            // To get index of current tab use tabController.index
+          }
+        });
+        return Scaffold(
           appBar: AppBar(
             bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.directions_car)),
-                Tab(icon: Icon(Icons.directions_transit)),
-                Tab(icon: Icon(Icons.directions_bike)),
-              ],
+              tabs: tabs,
             ),
-            title: Text('Tabs Demo'),
           ),
           body: TabBarView(
-            children: [
-              Icon(Icons.directions_car),
-              Icon(Icons.directions_transit),
-              Icon(Icons.directions_bike),
-            ],
+            children: tabs.map((Tab tab) {
+              return Center(
+                child: Text(
+                  tab.text + ' Tab',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+              );
+            }).toList(),
           ),
-        ),
-      ),
+        );
+      }),
     );
   }
 }
