cb09fb2604d6abe81623e0ea803ad725ce098355
nvrr <sureshvarma809@gmail.com>
2020-09-27 22:39:17 +0530

    SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,90 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
-class Todo {
-  final String title;
-  final String description;
-
-  Todo(this.title, this.description);
-}
-
+import 'dart:async';
 void main() {
   runApp(MaterialApp(
-    title: 'Passing Data',
-    home: TodosScreen(
-      todos: List.generate(
-        20,
-        (i) => Todo(
-          'Todo $i',
-          'A description of what needs to be done for Todo $i',
-        ),
-      ),
-    ),
+    theme: ThemeData(primaryColor: Colors.red, accentColor: Colors.yellowAccent),
+    debugShowCheckedModeBanner: false,
+    home: SplashScreen(),
   ));
 }
 
-class TodosScreen extends StatelessWidget {
-  final List<Todo> todos;
-
-  TodosScreen({Key key, @required this.todos}) : super(key: key);
-
+class SplashScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Todos'),
-      ),
-      body: ListView.builder(
-        itemCount: todos.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(todos[index].title),
-            // When a user taps the ListTile, navigate to the DetailScreen.
-            // Notice that you're not only creating a DetailScreen, you're
-            // also passing the current todo through to it.
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DetailScreen(),
-                  // Pass the arguments as part of the RouteSettings. The
-                  // DetailScreen reads the arguments from these settings.
-                  settings: RouteSettings(
-                    arguments: todos[index],
-                  ),
-                ),
-              );
-            },
-          );
-        },
-      ),
-    );
-  }
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
-class DetailScreen extends StatelessWidget {
+class _SplashScreenState extends State<SplashScreen> {
   @override
-  Widget build(BuildContext context) {
-    final Todo todo = ModalRoute.of(context).settings.arguments;
 
-    // Use the Todo to create the UI.
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 5), () => print('Splsh Done!')
+      );
+  }
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(todo.title),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Text(todo.description),
-      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.green),
+
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor:  Colors.white,
+                        radius: 50.0,
+                        child: Icon(
+                          Icons.shopping_cart,
+                          color: Colors.redAccent,
+                          size: 50.0,
+                        )
+                      ),
+                      Padding(padding: EdgeInsets.only(top: 10.0)),
+                      Text(
+                        "EcomEcom",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold
+                        )
+                      )
+                    ],
+                  )
+                )
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                    ),
+                    Text("Online Store \n for Everyone",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 10.0,
+                      fontWeight: FontWeight.bold
+                    )
+                    )
+                  ],
+                )
+              )
+            ],
+          )
+        ],
+      )
     );
   }
 }
